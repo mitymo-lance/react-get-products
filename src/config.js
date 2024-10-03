fa62f90f04ce0665a12118fc313acab4014059c1
Lance Eppley <lance@mitymo.com>
2024-10-03 11:35:43 -0400

    switched to production to test on cloudflare

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { api_urls } from  "./_api_urls.js";
 
-const environment = 'development';
+const environment = 'production';
 
 export const categories_url = api_urls.find((u) =>  u.name == 'categories' && u.environment == environment ).url;
 export const products_url = api_urls.find((u) => u.name == 'products' && u.environment == environment ).url;
